986358254efd9542bf6e77438f0cee97d0f3ba54
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-02 03:13:30 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -18,6 +18,7 @@ def hopper
 
 	 programmer_hash[:grace_hopper]
 end
+binding.pry
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
