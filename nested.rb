fd3b03430a96c61133989a265e83be621b270bbb
Zhengxiang Gong <jessiegong92@gmail.com>
2018-04-02 02:49:52 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-	 programmer_hash[:dennis_ritchies][:language]
+	 programmer_hash[:dennis_ritchies][:language][0]
 end
 
 def adding_matz
@@ -81,7 +81,10 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+	 programmer_hash[:yukihiro_matsumoto] = {
+		 :known_for => "Ruby",
+		 :languages => ["LISP", "C"]
+	 }
 
 end
 
